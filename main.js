42c53746f331351fa2768da8965fdfb6184812cd
谢宇 <xy13360717479@163.com>
2022-06-23 09:31:22 +0800

    完成了tabbar的开发

--- main.js
+++ main.js
@@ -2,6 +2,21 @@
 // #ifndef VUE3
 import Vue from 'vue'
 import App from './App'
+// 导入相关的包的赋值
+import {$http} from '@escook/request-miniprogram'
+uni.$http = $http
+
+// 请求开始之前做一些事情
+$http.beforeRequest = function (options) {
+  uni.showLoading({
+    title: '数据加载中...',
+  })
+}
+
+// 请求完成之后做一些事情
+$http.afterRequest = function () {
+  uni.hideLoading()
+}
 
 Vue.config.productionTip = false
 
